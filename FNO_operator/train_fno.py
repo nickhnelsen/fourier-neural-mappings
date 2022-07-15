c3cd45731e74275a693b5bb428fa5376ecaa3853
nnelsen <nnelsen@caltech.edu>
2022-07-15 16:28:12 -0700

    setup exps

--- FNO_operator/train_fno.py
+++ FNO_operator/train_fno.py
@@ -16,23 +16,24 @@ from timeit import default_timer
 # TODO: add command line
 # Process command line arguments
 print(sys.argv)
-# data_suffix = sys.argv[1]   # e.g., 'nu_inf_ell_p05_torch/2d/' or 'nu_1p5_ell_p25_torch/1000d/'
+# data_suffix = sys.argv[1]   # e.g., 'nu_inf_ell_p05_torch/' or 'nu_1p5_ell_p25_torch/'
 # N_train = int(sys.argv[2])  # number of solves (training sample size)
-# save_suffix = sys.argv[3]   # e.g., robustness, scalability, efficiency
-data_suffix = 'nu_inf_ell_p25_torch/2d/'
-save_suffix = '_TEST_bothVel/'
-save_prefix = 'results/'        # e.g., robustness, scalability, efficiency
+# save_prefix = sys.argv[3]   # e.g., robustness, scalability, efficiency
+save_prefix = 'robustness_TEST/'    # e.g., robustness, scalability, efficiency
+data_suffix = 'nu_inf_ell_p25_torch/'
 N_train = 1000
-sigma = 0 # TODO: noise stdev
+d_str = '1000'
+sigma = 0                   # index between 0 and 8
+
+# TODO: MC loop
 
 # File I/O
 data_prefix = '/media/nnelsen/SharedNHN/documents/datasets/Sandia/raise/training/'      # local
 # data_prefix = '/groups/astuart/nnelsen/data/raise/training/'                            # HPC
 FLAG_save_model = True
-FLAG_reduce = False
 
 # Sample size  
-N_test = 100
+N_test = 100        # number of validation samples to monitor during training
 
 # Resolution subsampling
 sub_in = 2**6       # input subsample factor (power of two) from s_max_in = 4097
@@ -45,7 +46,7 @@ width = 32
 
 # Training
 batch_size = 20
-epochs = 100
+epochs = 150
 learning_rate = 1e-3
 weight_decay = 1e-4
 scheduler_step = 100
@@ -60,32 +61,39 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print("Device is", device)
 
 # File IO
-data_folder = data_prefix + data_suffix
-savepath = './' + save_prefix + data_suffix[:-1] + save_suffix
+obj_suffix = '_n' + str(N_train) + '_d' + d_str + '_s' + str(sigma) + '.pt'
+data_folder = data_prefix + data_suffix + d_str + 'd/'
+savepath = './results/' + save_prefix + data_suffix
 os.makedirs(savepath, exist_ok=True)
 
-# TODO: monitor qoi during training?
-# TODO: add noise flag to outputs
+# Load
 y_train = torch.load(data_folder + 'velocity.pt')['velocity'][:,::sub_in]
 N_max, s = y_train.shape
 assert max(N_train, N_test) <= N_max
 x_train = torch.zeros(N_max, 2, s)
 x_train[:, 0, :] = y_train
 y_train = torch.load(data_folder + 'state.pt')['state'][:,::sub_out,::sub_out,-1] # final time state only
-# qoi_test = torch.load(data_folder + 'qoi.pt')['qoi']
 
-# TODO: torch.permute to get random training indices like in TORCH_RFM_UQ
+# Shuffle
+dataset_shuffle_idx = torch.randperm(N_max)
+torch.save({'shuffle_idx': dataset_shuffle_idx}, savepath + 'shuffle_idx' + obj_suffix)
+x_train = x_train[dataset_shuffle_idx, ...]
+y_train = y_train[dataset_shuffle_idx, ...]
+
+# Extract
 x_test = x_train[-N_test:,...].unsqueeze(-1).repeat(1, 1, 1, s) # velocity is constant in y=x_2 direction
 x_train = x_train[:N_train,...].unsqueeze(-1).repeat(1, 1, 1, s) # velocity is constant in y=x_2 direction
-
 y_test = y_train[-N_test:,...]
 y_train = y_train[:N_train,...]
-# qoi_test = qoi_test[-N_test:,...]
 
-# TODO: decide if normalizer is needed on inputs and/or outputs
-# x_normalizer = UnitGaussianNormalizer(x_train)
-# x_train = x_normalizer.encode(x_train)
-# x_test = x_normalizer.encode(x_test)
+# Noise
+stdevs = torch.arange(0.0, 2.01, 0.25)
+if sigma not in [i for i in range(stdevs.shape[0])]:
+    raise ValueError("sigma must be an index from 0 to 8")
+elif sigma > 0: # add noise in the output space
+    stdev = stdevs[sigma]
+    y_test = y_test + stdev*torch.randn(y_test.shape)
+    y_train = y_train + stdev*torch.randn(y_train.shape)
 
 train_loader = DataLoader(TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
@@ -98,19 +106,15 @@ test_loader = DataLoader(TensorDataset(x_test, y_test), batch_size=batch_size, s
 s_outputspace = tuple(y_train.shape[-2:])   # same output shape as the output dataset
 
 model = FNO2d(modes1, modes2, width, s_outputspace).to(device)
-print(count_params(model))
+print("FNO parameter count:", count_params(model))
 
 optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
-if FLAG_reduce:
-    scheduler_val = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=scheduler_gamma)
 
 loss_f = LpLoss(size_average=False)
 
 errors = torch.zeros((epochs, 2))
 
-# TODO: decide if early stopping is needed
-# lowest_test = 10.0 # initialize a test loss threshold
 t0 = default_timer()
 for ep in range(epochs):
     t1 = default_timer()
@@ -145,21 +149,16 @@ for ep in range(epochs):
     test_loss /= N_test
     
     scheduler.step()
-    if FLAG_reduce:
-        scheduler_val.step(test_loss)
 
     errors[ep,0] = train_loss
     errors[ep,1] = test_loss
 
     if FLAG_save_model:
-        torch.save(model.state_dict(), savepath + 'model_N' + str(N_train) + '.pt')
-        # if test_loss < lowest_test:
-        #     torch.save(model.state_dict(), 'model.pt')
-        #     lowest_test = test_loss
+        torch.save(model.state_dict(), savepath + 'model' + obj_suffix)
 
     t2 = default_timer()
 
     print("Epoch:", ep, "Train L2:", train_loss, "Test L2:", test_loss, "Epoch time:", t2-t1)
-    torch.save({'errors': errors}, savepath + 'errors_N' + str(N_train) + '.pt')
+    torch.save({'errors': errors}, savepath + 'errors' + obj_suffix)
 
 print("Total time elapsed (min):", (default_timer()-t0)/60., "Total epochs trained:", epochs)
