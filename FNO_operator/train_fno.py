a7763d30b9893e124edada887336aac43a084544
nnelsen <nnelsen@caltech.edu>
2022-07-15 19:38:10 -0700

    eval progress

--- FNO_operator/train_fno.py
+++ FNO_operator/train_fno.py
@@ -3,10 +3,17 @@ import torch
 from models import FNO2d
 import os, sys; sys.path.append(os.path.join('..'))
 from util.Adam import Adam
-from util.utilities_module import LpLoss, UnitGaussianNormalizer, count_params
+from util.utilities_module import LpLoss, LppLoss, count_params, validate, dataset_with_indices
 from torch.utils.data import TensorDataset, DataLoader
-
+TensorDatasetID = dataset_with_indices(TensorDataset)
 from timeit import default_timer
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+plt.rcParams['figure.figsize'] = [6.0, 4.0]
+plt.rcParams['font.size'] = 16
+plt.rcParams['figure.dpi'] = 250
+plt.rcParams['savefig.dpi'] = 250
 
 ################################################################
 #
@@ -22,7 +29,7 @@ print(sys.argv)
 # d_str = sys.argv[4]         # KLE dimension of training inputs
 # sigma = int(sys.argv[5])    # index between 0 and 8 that defines the noise standard deviation
 
-save_prefix = 'robustness_TEST/'    # e.g., robustness, scalability, efficiency
+save_prefix = 'robustness_TEST_eval/'    # e.g., robustness, scalability, efficiency
 data_suffix = 'nu_inf_ell_p25_torch/'
 N_train = 1000
 d_str = '1000'
@@ -30,10 +37,11 @@ sigma = 0                   # index between 0 and 8
 
 # TODO: MC loop
 
-# File paths
+# File I/O
 data_prefix = '/media/nnelsen/SharedNHN/documents/datasets/Sandia/raise/training/'      # local
 # data_prefix = '/groups/astuart/nnelsen/data/raise/training/'                            # HPC
 FLAG_save_model = True
+FLAG_save_plots = True
 
 # Sample size  
 N_test = 100        # number of validation samples to monitor during training
@@ -49,7 +57,7 @@ width = 32
 
 # Training
 batch_size = 20
-epochs = 150
+epochs = 15
 learning_rate = 1e-3
 weight_decay = 1e-4
 scheduler_step = 100
@@ -162,7 +170,7 @@ for ep in range(epochs):
     t2 = default_timer()
 
     print("Epoch:", ep, "Train L2:", train_loss, "Test L2:", test_loss, "Epoch time:", t2-t1)
-    torch.save({'errors': errors}, savepath + 'errors' + obj_suffix)
+    torch.save({'train_errors': errors}, savepath + 'train_errors' + obj_suffix)
 
 print("Total time elapsed (min):", (default_timer()-t0)/60., "Total epochs trained:", epochs)
 
@@ -175,7 +183,12 @@ data_prefix = '/media/nnelsen/SharedNHN/documents/datasets/Sandia/raise/validati
 # data_prefix = '/groups/astuart/nnelsen/data/raise/validation/'                            # HPC
 
 # File IO
-data_folder = data_prefix + d_str + 'd_torch/'
+if d_str == '1000':
+    d_test_str = '2'    # test on d=2 case to show resolution-invariance
+else:
+    d_test_str = d_str
+data_folder = data_prefix + d_test_str + 'd_torch/'
+obj_suffix = '_TESTd' + d_test_str + obj_suffix
 
 # Load
 y_test = torch.load(data_folder + 'velocity.pt')['velocity'][:,::sub_in]
@@ -186,9 +199,72 @@ y_test = torch.load(data_folder + 'state.pt')['state'][:,::sub_out,::sub_out,-1]
 qoi_test = torch.load(data_folder + 'qoi.pt')['qoi']
 
 # Process
-x_test = x_test.unsqueeze(-1).repeat(1, 1, 1, s) # velocity is constant in y=x_2 direction
+x_test = x_test.unsqueeze(-1).repeat(1, 1, 1, s_test) # velocity is constant in y=x_2 direction
+test_loader = DataLoader(TensorDatasetID(x_test, y_test), batch_size=batch_size, shuffle=False)
 
-test_loader = DataLoader(TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
+# Evaluate
+model.eval()
+loss_vec = LpLoss(size_average=False, reduction=False) # relative L^2 error (not summed)
+loss_abs = LppLoss(size_average=False, reduction=True) # absolute squared L^2 error
 
+t1 = default_timer()
+er_test_loss = 0.0
+num = 0.0
+den = 0.0
+test_out = torch.zeros(y_test.shape)
+qoi_out = torch.zeros(N_test_max)
+idx_qoi = torch.tensor(s_outputspace)//2 + 1
+errors_test = torch.zeros(y_test.shape[0])
+with torch.no_grad():
+    for x, y, idx_test in test_loader:
+        x, y = x.to(device), y.to(device)
 
+        out = model(x)
 
+        er_test_loss += loss_f(out, y).item()
+        num += loss_abs.abs(out, y).item()
+        den += loss_abs.abs(y, 0*y).item()
+        
+        errors_test[idx_test] = loss_vec(out, y).cpu()
+        
+        out = out.squeeze().cpu()
+        test_out[idx_test, ...] = out
+        qoi_out[idx_test] = out[..., idx_qoi[-2], idx_qoi[-1]]
+
+er_test_loss /= N_test_max
+er_test_bochner = (num/den)**(0.5)
+er_test_qoi = validate(qoi_test, qoi_out)
+t2 = default_timer()
+print("Time to evaluate", N_test_max, "samples (sec):", t2-t1)
+print("Average relative L2 test:", er_test_loss, "Relative Bochner L2 test:", er_test_bochner)
+print("Relative L2 QoI test error:", er_test_qoi)
+
+# Save test errors
+test_errors = [er_test_qoi, er_test_bochner, er_test_loss]
+torch.save({'qoi_bochner_loss': test_errors, 'test_list': errors_test},\
+           savepath + 'test_errors' + obj_suffix)
+
+# TODO: remove for public version of code
+# Evaluate trained model on 2D parameter grid
+x_test = torch.load(data_prefix + '2d_qoi_plot/' + 'velocity.pt')['velocity'][:,::sub_in]
+N_grid, s_grid = x_test.shape
+x_grid = torch.zeros(N_grid, 2, s_grid)
+x_grid[:, 0, :] = x_test
+x_grid = x_grid.unsqueeze(-1).repeat(1, 1, 1, s_grid) # velocity is constant in y=x_2 direction
+grid_loader = DataLoader(TensorDatasetID(x_grid, 0*x_grid), batch_size=batch_size, shuffle=False)
+qoi_grid = torch.zeros(x_grid.shape[0])
+with torch.no_grad():
+    for x, _, idx_grid in grid_loader:
+        x = x.to(device)
+        qoi_grid[idx_grid] = model(x).squeeze().cpu()[..., idx_qoi[-2], idx_qoi[-1]]
+torch.save({'qoi_grid': qoi_grid}, savepath + 'qoi_grid' + obj_suffix)
+grid = torch.load(data_prefix + '2d_qoi_plot/' + 'params.pt')['params']
+grid = grid.reshape(s_outputspace[-2], s_outputspace[-1], -1)
+plt.contourf(grid[...,-2], grid[...,-1],\
+             qoi_grid.reshape(s_outputspace[-2], s_outputspace[-1]), cmap=cm.viridis)
+plt.title(r'FNO $(N=%d, d_{\mathrm{tr}}=%d, \sigma=0)$' % (N_train, int(d_str)))
+plt.xlabel(r'$\xi_1$')
+plt.ylabel(r'$\xi_2$')
+plt.colorbar(label=r'QoI')
+if FLAG_save_plots:
+    plt.savefig(savepath + 'qoi_grid' + obj_suffix[:-2] + 'pdf', format='pdf')
