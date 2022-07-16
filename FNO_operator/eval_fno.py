69366af8eebdcb7a8da791bbe45528d53b234191
nnelsen <nnelsen@caltech.edu>
2022-07-15 19:38:38 -0700

    comment to make this file eval_all on all d vals

--- FNO_operator/eval_fno.py
+++ FNO_operator/eval_fno.py
@@ -16,22 +16,28 @@ from timeit import default_timer
 # user configuration
 #
 ################################################################
-# File I/O
-data_folder = '/media/nnelsen/SharedNHN/documents/datasets/eit/'    # local
-# data_folder = '/groups/astuart/nnelsen/data/eit/'                    # HPC
+# Process command line arguments
+print(sys.argv)
+save_prefix = sys.argv[1]   # e.g., robustness, scalability, efficiency
+data_suffix = sys.argv[2]   # e.g., 'nu_inf_ell_p05_torch/' or 'nu_1p5_ell_p25_torch/'
+N_train = int(sys.argv[3])  # training sample size
+d_str = sys.argv[4]         # KLE dimension of training inputs
+sigma = int(sys.argv[5])    # index between 0 and 8 that defines the noise standard deviation
 
-# Sample size
-N_train = 9500      # N_train_max = 10000
-N_test = 500
+# File I/O
+data_prefix = '/media/nnelsen/SharedNHN/documents/datasets/Sandia/raise/validation/'      # local
+# data_prefix = '/groups/astuart/nnelsen/data/raise/validation/'                            # HPC
 
 # Resolution subsampling
-sub_in = 2**1       # input subsample factor (power of two) from s_max_out = 512
-sub_out = 2**0      # output subsample factor (power of two) from s_max_out = 256
+sub_in = 2**6       # input subsample factor (power of two) from s_max_in = 4097
+sub_out = 2**0      # output subsample factor (power of two) from s_max_out = 33
 
 # FNO
 modes1 = 12
 modes2 = 12
 width = 32
+
+# TODO: loop to evaluate on all test d values
 
 # Eval
 batch_size = 20
@@ -43,13 +49,44 @@ batch_size = 20
 ################################################################
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-x_test3 = torch.load(data_folder + 'kernel_3heart_rhop7.pt')['kernel_3heart'][...,::sub_in,::sub_in]
-y_test3 = torch.load(data_folder + 'conductivity_3heart_rhop7.pt')['conductivity_3heart'][...,::sub_out,::sub_out]
+# File IO
+obj_suffix = '_n' + str(N_train) + '_d' + d_str + '_s' + str(sigma) + '.pt'
+data_folder = data_prefix + d_str + 'd_torch/'
+savepath = './results/' + save_prefix + data_suffix
+os.makedirs(savepath, exist_ok=True)
 
-# x_test3 = torch.load(data_folder + 'kernel_3heart.pt')['kernel_3heart'][...,::sub_in,::sub_in]
-# y_test3 = torch.load(data_folder + 'conductivity_3heart.pt')['conductivity_3heart'][...,::sub_out,::sub_out]
+# Load
+y_train = torch.load(data_folder + 'velocity.pt')['velocity'][:,::sub_in]
+N_max, s = y_train.shape
+assert max(N_train, N_test) <= N_max
+x_train = torch.zeros(N_max, 2, s)
+x_train[:, 0, :] = y_train
+y_train = torch.load(data_folder + 'state.pt')['state'][:,::sub_out,::sub_out,-1] # final time state only
 
-y_test3 = torch.flip(y_test3, [-2])
+# Shuffle
+dataset_shuffle_idx = torch.randperm(N_max)
+torch.save({'shuffle_idx': dataset_shuffle_idx}, savepath + 'shuffle_idx' + obj_suffix)
+x_train = x_train[dataset_shuffle_idx, ...]
+y_train = y_train[dataset_shuffle_idx, ...]
+
+# Extract
+x_test = x_train[-N_test:,...].unsqueeze(-1).repeat(1, 1, 1, s) # velocity is constant in y=x_2 direction
+x_train = x_train[:N_train,...].unsqueeze(-1).repeat(1, 1, 1, s) # velocity is constant in y=x_2 direction
+y_test = y_train[-N_test:,...]
+y_train = y_train[:N_train,...]
+
+# Noise
+stdevs = torch.arange(0.0, 2.01, 0.25)
+if sigma not in [i for i in range(stdevs.shape[0])]:
+    raise ValueError("sigma must be an index from 0 to 8")
+elif sigma > 0: # add noise in the output space
+    stdev = stdevs[sigma]
+    y_test = y_test + stdev*torch.randn(y_test.shape)
+    y_train = y_train + stdev*torch.randn(y_train.shape)
+
+train_loader = DataLoader(TensorDataset(x_train, y_train), batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(TensorDataset(x_test, y_test), batch_size=batch_size, shuffle=False)
+
 
 x_train = torch.load(data_folder + 'kernel.pt')['kernel'][...,::sub_in,::sub_in]
 y_train = torch.load(data_folder + 'conductivity.pt')['conductivity'][...,::sub_out,::sub_out]
@@ -76,7 +113,7 @@ test_loader = DataLoader(TensorDatasetID(x_test, y_test), batch_size=batch_size,
 
 ################################################################
 #
-# evaluation on train and test sets
+# evaluation on all test sets with the same model
 #
 ################################################################
 s_outputspace = tuple(y_train.shape[-2:])   # same output shape as the output dataset
@@ -88,26 +125,6 @@ print(count_params(model))
 model.eval()
 loss_f = LpLoss(size_average=False)
 loss_vec = LpLoss(size_average=False, reduction=False)
-
-t1 = default_timer()
-train_loss = 0.0
-out_train = torch.zeros(y_train.shape)
-errors_train = torch.zeros(y_train.shape[0])
-with torch.no_grad():
-    for x, y, idx_train in train_loader:
-        x, y = x.to(device), y.to(device)
-
-        out = model(x)*mask + ~mask # set model to one outside unit disk of radius 1
-
-        train_loss += loss_f(out, y).item()
-        
-        errors_train[idx_train] = loss_vec(out, y).cpu()
-        
-        out_train[idx_train,...] = out.squeeze().cpu()
-
-train_loss /= N_train
-t2 = default_timer()
-print(train_loss, t2-t1)
 
 t1 = default_timer()
 test_loss = 0.0
@@ -136,54 +153,7 @@ print(test_loss, t2-t1)
 ################################################################
 plot_folder = "figures/"
 os.makedirs(plot_folder, exist_ok=True)
-mask_plot = torch.clone(mask).cpu()
-out_train[:,~mask_plot] = float('nan')
-out_test[:,~mask_plot] = float('nan')
 
-# Phantom three evaluations
-with torch.no_grad():
-    out3 = model(x_test3.to(device))*mask + ~mask
-    out3 = out3.squeeze().cpu()
-out3[:,~mask_plot] = float('nan')
-
-# %% Worst, median, best case inputs (train)
-plt.close("all")
-
-idx_worst = torch.argmax(errors_train).item()
-idx_median = torch.argsort(errors_train)[errors_train.shape[0]//2].item()
-idx_best = torch.argmin(errors_train).item()
-
-idxs = [idx_worst, idx_median, idx_best]
-names = ["worst", "median", "best"]
-
-for i in range(3):
-    idx = idxs[i]
-    true_trainsort = y_train[idx,...].squeeze()
-    true_trainsort[~mask_plot] = float('nan')
-    plot_trainsort = out_train[idx,...].squeeze()
-    er_trainsort = torch.abs(plot_trainsort - true_trainsort).squeeze()
-    
-    plt.close()
-    plt.figure(3, figsize=(9, 9))
-    plt.subplot(2,2,1)
-    plt.title('Train Output')
-    plt.imshow(plot_trainsort, origin='lower', interpolation='none')
-    plt.box(False)
-    plt.subplot(2,2,2)
-    plt.title('Train Truth')
-    plt.imshow(true_trainsort, origin='lower', interpolation='none')
-    plt.box(False)
-    plt.subplot(2,2,3)
-    plt.title('Train Input')
-    plt.imshow(x_train[idx,...].squeeze(), origin='lower')
-    plt.subplot(2,2,4)
-    plt.title('Train PW Error')
-    plt.imshow(er_trainsort, origin='lower')
-    plt.box(False)
-    plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
-    plt.tight_layout()
-
-    # plt.savefig(plot_folder + "eval_train_" + names[i] + ".png", format='png', dpi=300, bbox_inches='tight')
     
 # %% Worst, median, best case inputs (test)
 plt.close("all")
@@ -223,36 +193,6 @@ for i in range(3):
     plt.tight_layout()
 
     # plt.savefig(plot_folder + "eval_test_" + names[i] + ".png", format='png', dpi=300, bbox_inches='tight')
-
-# %% Train point
-plt.close("all")
-
-pind_train = torch.randint(N_train, [1]).item()
-
-true_train = y_train[pind_train,...].squeeze()
-true_train[~mask_plot] = float('nan')
-plot_train = out_train[pind_train,...].squeeze()
-er_train = torch.abs(plot_train - true_train).squeeze()
-
-plt.figure(1, figsize=(9, 9))
-plt.subplot(2,2,1)
-plt.title('Train Output')
-plt.imshow(plot_train, origin='lower', interpolation='none')
-plt.box(False)
-plt.subplot(2,2,2)
-plt.title('Train Truth')
-plt.imshow(true_train, origin='lower', interpolation='none')
-plt.box(False)
-plt.subplot(2,2,3)
-plt.title('Train Input')
-plt.imshow(x_train[pind_train,...].squeeze(), origin='lower')
-plt.subplot(2,2,4)
-plt.title('Train PW Error')
-plt.imshow(er_train, origin='lower')
-plt.box(False)
-plt.setp(plt.gcf().get_axes(), xticks=[], yticks=[])
-plt.tight_layout()
-plt.show()
 
 # %% Save train or not
 # plt.savefig(plot_folder + "eval_train" + str(pind_train) + ".png", format='png', dpi=300, bbox_inches='tight')
