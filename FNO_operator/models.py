6298e73c76a7bbd97ef4a3ecf4f926087a7b4c4c
nnelsen <nnelsen@caltech.edu>
2022-07-21 17:25:01 -0700

    fix projector and padding errors

--- FNO_operator/models.py
+++ FNO_operator/models.py
@@ -3,8 +3,27 @@ import torch.nn as nn
 import torch.fft as fft
 import torch.nn.functional as F
 
+import os, sys; sys.path.append(os.path.join('..', 'util'))
+from utilities_module import resize_rfft, resize_fft
+
+def resize_rfft2(ar, s):
+    """
+    Truncates or zero pads the highest frequencies of ``ar'' such that torch.fft.irfft2(ar, s=s) is either an interpolation to a finer grid or a subsampling to a coarser grid.
+    Args
+        ar: (n, c, N_1, N_2) tensor, must satisfy real conjugate symmetry (not checked)
+        s: (2) tuple, s=(s_1, s_2) desired irfft2 output dimension (s_i >=1)
+    Output
+        out: (n, c, s1, s_2//2 + 1) tensor
+    """
+    s1, s2 = s
+    out = resize_rfft(ar, s2) # last axis (rfft)
+    return resize_fft(out.permute(0, 1, 3, 2), s1).permute(0, 1, 3, 2) # second to last axis (fft)
+
 class SpectralConv2d(nn.Module):
     def __init__(self, in_channels, out_channels, modes1, modes2):
+        """
+        Fourier integral operator layer defined for functions over the torus
+        """
         super(SpectralConv2d, self).__init__()
 
         self.in_channels = in_channels
@@ -18,40 +37,55 @@ class SpectralConv2d(nn.Module):
         self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
         self.weights2 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, self.modes2, dtype=torch.cfloat))
 
-    # Complex multiplication
-    def compl_mul2d(self, input_batch, weights):
-        # (batch, in_channel, x,y ), (in_channel, out_channel, x,y) -> (batch, out_channel, x,y)
-        return torch.einsum("bixy,ioxy->boxy", input_batch, weights)
+    def compl_mul2d(self, input_tensor, weights):
+        """
+        Complex multiplication:
+        (batch, in_channel, nx, ny), (in_channel, out_channel, nx, ny) -> (batch, out_channel, nx, ny)
+        """
+        return torch.einsum("bixy,ioxy->boxy", input_tensor, weights)
 
     def forward(self, x, s=None):
-        # Compute Fourier coeffcients
-        x_ft = fft.rfft2(x)
+        """
+        Input shape (of x):     (batch, channels=2, nx_in, ny_in)
+        s:                      (list or tuple, length 2): desired spatial resolution (s,s) in output space
+        """
+        # Original resolution
+        xsize = x.shape[-2:]
+        
+        # Compute Fourier coeffcients (un-scaled)
+        x = fft.rfft2(x)
 
         # Multiply relevant Fourier modes
-        out_ft = torch.zeros(x.shape[0], self.out_channels,  x.size(-2), x.size(-1)//2 + 1, dtype=torch.cfloat, device=x.device)
+        out_ft = torch.zeros(x.shape[0], self.out_channels, xsize[-2], xsize[-1]//2 + 1, dtype=torch.cfloat, device=x.device)
         out_ft[:, :, :self.modes1, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :, :self.modes1, :self.modes2], self.weights1)
+            self.compl_mul2d(x[:, :, :self.modes1, :self.modes2], self.weights1)
         out_ft[:, :, -self.modes1:, :self.modes2] = \
-            self.compl_mul2d(x_ft[:, :, -self.modes1:, :self.modes2], self.weights2)
+            self.compl_mul2d(x[:, :, -self.modes1:, :self.modes2], self.weights2)
 
         # Return to physical space
-        if s is None:
-            x = fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+        if s is None or tuple(s) == tuple(xsize):
+            x = fft.irfft2(out_ft, s=tuple(xsize))
         else:
-            x = fft.irfft2(out_ft, s=s)
+            x = fft.irfft2(resize_rfft2(out_ft, s), s=s, norm="forward") / (xsize[-2] * xsize[-1])
 
         return x
 
 class FNO2d(nn.Module):
-    def __init__(self, modes1, modes2, width, s_outputspace, width_final=128, padding=8, d_in=4, d_out=1):
+    def __init__(self, modes1, modes2, width,
+                 s_outputspace=None,
+                 width_final=128,
+                 padding=8,
+                 d_in=4,
+                 d_out=1
+                 ):
         """
-        modes1, modes2 (int): Fourier mode truncation levels
-        width (int): dimension of channel space
-        s_outputspace (list or tuple, length 2): desired spatial resolution (s,s) in output space
-        width_final (int): width of the final projection layer
-        padding (int or float): (1.0/padding) is fraction of domain to zero pad (the input is not periodic)
-        d_in  (int): nummber of input channels (here 2 velocity inputs + 2 space variables)
-        d_out (int): one output channel (co-domain dimension of output space functions)
+        modes1, modes2  (int): Fourier mode truncation levels
+        width           (int): constant dimension of channel space
+        s_outputspace   (list or tuple, length 2): desired spatial resolution (s,s) in output space
+        width_final     (int): width of the final projection layer
+        padding         (int or float): (1.0/padding) is fraction of domain to zero pad (non-periodic)
+        d_in            (int): number of input channels (here 2 velocity inputs + 2 space variables)
+        d_out           (int): one output channel (co-domain dimension of output space functions)
         """
         super(FNO2d, self).__init__()
 
@@ -62,8 +96,8 @@ class FNO2d(nn.Module):
         self.padding = padding
         self.d_in = d_in
         self.d_out = d_out 
-        self.num_pad_outputspace = tuple([s//self.padding for s in list(s_outputspace)])
-        self.s_outputspace = tuple([s + s//self.padding for s in list(s_outputspace)])
+        
+        self.set_outputspace_resolution(s_outputspace)
 
         self.fc0 = nn.Linear(self.d_in, self.width)
 
@@ -82,17 +116,23 @@ class FNO2d(nn.Module):
 
     def forward(self, x):
         """
-        Input shape: (batch, channels=1, x, y)
-        Output shape: (batch, channels=1, x, y)
+        Input shape (of x):     (batch, channels=2, nx_in, ny_in)
+        Output shape:           (batch, channels=1, nx_out, ny_out)
+        
+        The input resolution is determined by x.shape[-2:]
+        The output resolution is determined by self.s_outputspace
         """
         # Lifting layer
+        x_res = x.shape[-2:]
         x = x.permute(0, 2, 3, 1)
-        x = torch.cat((x, self.get_grid(x.shape, x.device)), dim=-1)
+        x = torch.cat((x, self.get_grid(x.shape, x.device)), dim=-1)    # grid ``features''
         x = self.fc0(x)
         x = x.permute(0, 3, 1, 2)
-        x = F.pad(x, [0, x.shape[-1]//self.padding, 0, x.shape[-2]//self.padding])
+        
+        # Map from input domain into the torus
+        x = F.pad(x, [0, x_res[-1]//self.padding, 0, x_res[-2]//self.padding])
 
-        # Four Fourier integral operator layers
+        # Fourier integral operator layers on the torus
         x1 = self.conv0(x)
         x2 = self.w0(x)
         x = x1 + x2
@@ -108,21 +148,29 @@ class FNO2d(nn.Module):
         x = x1 + x2
         x = F.gelu(x)
 
-        x1 = self.conv3(x, s=self.s_outputspace)
-        x2 = self.w3(x)
-        x2 = self.projector(x2, s=self.s_outputspace)
+        x1 = self.conv3(x, s=self.s_outputspace)        # change resolution in consistent way
+        x2 = self.projector(x, s=self.s_outputspace)    # change resolution in consistent way
+        x2 = self.w3(x2)
         x = x1 + x2
 
+        # Map from the torus into the output domain
+        if self.s_outputspace is not None:
+            x = x[..., :-self.num_pad_outputspace[-2], :-self.num_pad_outputspace[-1]]
+        else:
+            x = x[..., :-x_res[-2]//self.padding, :-x_res[-1]//self.padding]
+        
         # Final projection layer
-        x = x[..., :-self.num_pad_outputspace[-2], :-self.num_pad_outputspace[-1]]
         x = x.permute(0, 2, 3, 1)
         x = self.fc1(x)
         x = F.gelu(x)
         x = self.fc2(x)
-        
+
         return x.permute(0, 3, 1, 2)
     
     def get_grid(self, shape, device):
+        """
+        Returns a discretization of the 2D identity function on [0,1]^2
+        """
         batchsize, size_x, size_y = shape[0], shape[1], shape[2]
         gridx = torch.linspace(0, 1, size_x)
         gridx = gridx.reshape(1, size_x, 1, 1).repeat([batchsize, 1, size_y, 1])
@@ -132,9 +180,20 @@ class FNO2d(nn.Module):
     
     def projector(self, x, s=None):
         """
-        Either truncate or zero pad the Fourier modes of x to resolution s
+        Either truncate or zero pad the Fourier modes of x so that x has new resolution s
         """
-        if s is not None:
-            x = fft.irfft2(fft.rfft2(x), s=s)
+        if s is not None and tuple(s) != tuple(x.shape[-2:]):
+            x = fft.irfft2(resize_rfft2(fft.rfft2(x, norm="forward"), s), s=s, norm="forward")
             
         return x
+
+    def set_outputspace_resolution(self, s=None):
+        """
+        Helper to set desired output space resolution of the model at any time
+        """
+        if s is None:
+            self.s_outputspace = None
+            self.num_pad_outputspace = None
+        else:
+            self.s_outputspace = tuple([r + r//self.padding for r in list(s)])
+            self.num_pad_outputspace = tuple([r//self.padding for r in list(s)])
