9dedf79da063fc1508a3338e85649d974067e7db
nnelsen <nnelsen@caltech.edu>
2022-07-27 12:32:27 -0700

    update train script for FNF

--- FNO_functionals/train_fnf.py
+++ FNO_functionals/train_fnf.py
@@ -3,7 +3,7 @@ import numpy as np
 import os, sys; sys.path.append(os.path.join('..', 'util'))
 from timeit import default_timer
 
-from models import FNO2d
+from models import FNF2d
 from Adam import Adam
 from utilities_module import LpLoss, LppLoss, count_params, validate, dataset_with_indices
 from torch.utils.data import TensorDataset, DataLoader
@@ -39,7 +39,7 @@ N_test = 100        # number of validation samples to monitor during training
 sub_in = 2**6       # input subsample factor (power of two) from s_max_in = 4097
 sub_out = 2**0      # output subsample factor (power of two) from s_max_out = 33
 
-# FNO
+# FNF
 modes1 = 12
 modes2 = 12
 width = 32
@@ -72,7 +72,7 @@ N_max, s = y_train_all.shape
 assert max(N_train, N_test) <= N_max
 x_train_all = torch.zeros(N_max, 2, s)
 x_train_all[:, 0, :] = y_train_all
-y_train_all = torch.load(data_folder + 'state.pt')['state'][:,::sub_out,::sub_out,-1] # final time state only
+y_train_all = torch.load(data_folder + 'qoi.pt')['qoi'].unsqueeze(-1)
 
 # Noise
 stdevs = torch.arange(0.0, 2.01, 0.25)
@@ -99,12 +99,10 @@ y_eval = torch.load(data_folder_eval + 'velocity.pt')['velocity'][:,::sub_in]
 N_eval_max, s_eval = y_eval.shape
 x_eval = torch.zeros(N_eval_max, 2, s_eval)
 x_eval[:, 0, :] = y_eval
-y_eval = torch.load(data_folder_eval + 'state.pt')['state'][:,::sub_out,::sub_out,-1]
 qoi_eval = torch.load(data_folder_eval + 'qoi.pt')['qoi']
+y_eval = qoi_eval.unsqueeze(-1)
 
 # Process evaluation
-s_outputtest = y_eval.shape[-2:]
-idx_qoi = torch.div(torch.tensor(s_outputtest), 2, rounding_mode="floor") # center of grid
 x_eval = x_eval.unsqueeze(-1).repeat(1, 1, 1, s_eval) # velocity is constant in y=x_2 direction
 eval_loader = DataLoader(TensorDatasetID(x_eval, y_eval), batch_size=batch_size, shuffle=False)
 
@@ -160,11 +158,9 @@ for loop in range(N_MC):
     #
     # training
     #
-    ################################################################
-    s_outputspace = tuple(y_train.shape[-2:])   # same output shape as the output dataset
-    
-    model = FNO2d(modes1, modes2, width, s_outputspace).to(device)
-    print("FNO parameter count:", count_params(model))
+    ################################################################    
+    model = FNF2d(modes1, modes2, width).to(device)
+    print("FNF parameter count:", count_params(model))
     
     optimizer = Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=scheduler_step, gamma=scheduler_gamma)
@@ -241,7 +237,6 @@ for loop in range(N_MC):
     er_test_loss = 0.0
     num = 0.0
     den = 0.0
-    state_out = torch.zeros(y_eval.shape)
     qoi_out = torch.zeros(N_eval_max)
     errors_test = torch.zeros(y_eval.shape[0])
     with torch.no_grad():
@@ -256,9 +251,7 @@ for loop in range(N_MC):
             
             errors_test[idx_test] = loss_vec(out, y).cpu()
             
-            out = out.squeeze().cpu()
-            state_out[idx_test, ...] = out
-            qoi_out[idx_test] = out[..., idx_qoi[-2], idx_qoi[-1]]
+            qoi_out[idx_test] = out.squeeze().cpu()
     
     er_test_loss /= N_eval_max
     er_test_bochner = (num/den)**(0.5)
@@ -281,7 +274,7 @@ for loop in range(N_MC):
     with torch.no_grad():
         for x, _, idx_grid in grid_loader:
             x = x.to(device)
-            qoi_grid[idx_grid] = model(x).squeeze().cpu()[..., idx_qoi[-2], idx_qoi[-1]]
+            qoi_grid[idx_grid] = model(x).squeeze().cpu()
     qoi_grid_all.append(qoi_grid.numpy())
     np.save(savepath + 'qoi_grid_all' + obj_suffix, np.asarray(qoi_grid_all))
 
@@ -316,6 +309,7 @@ if FLAG_save_plots:
     
     # TODO: remove for public version of code
     # Make 2D QoI grid plot
+    s_outputtest = (33, 33)
     grid = torch.load(data_prefix_eval + '2d_qoi_plot/' + 'params.pt')['params']
     grid = grid.reshape(s_outputtest[-2], s_outputtest[-1], -1)
     plt.close()
@@ -323,11 +317,11 @@ if FLAG_save_plots:
                   qoi_grid.reshape(s_outputtest), cmap=mpl.cm.viridis)
     fontdict = {'fontsize' : 16}
     if sigma in [0, 4, 8]:
-        plt.title(r'FNO $(N=%d, d_{\mathrm{tr}}=%d, \sigma=%d)$' % (N_train, int(d_str), stdev), fontdict=fontdict)
+        plt.title(r'FNF $(N=%d, d_{\mathrm{tr}}=%d, \sigma=%d)$' % (N_train, int(d_str), stdev), fontdict=fontdict)
     elif sigma in [2, 6]:
-        plt.title(r'FNO $(N=%d, d_{\mathrm{tr}}=%d, \sigma=%.1f)$' % (N_train, int(d_str), stdev), fontdict=fontdict)
+        plt.title(r'FNF $(N=%d, d_{\mathrm{tr}}=%d, \sigma=%.1f)$' % (N_train, int(d_str), stdev), fontdict=fontdict)
     else:
-        plt.title(r'FNO $(N=%d, d_{\mathrm{tr}}=%d, \sigma=%.2f)$' % (N_train, int(d_str), stdev), fontdict=fontdict)
+        plt.title(r'FNF $(N=%d, d_{\mathrm{tr}}=%d, \sigma=%.2f)$' % (N_train, int(d_str), stdev), fontdict=fontdict)
     plt.xlabel(r'$\xi_1$')
     plt.ylabel(r'$\xi_2$')
     plt.colorbar(label=r'QoI')
@@ -343,71 +337,15 @@ if FLAG_save_plots:
     XX = torch.linspace(0, 1, y_eval.shape[-1])
     (YY, XX) = torch.meshgrid(XX, XX)
     for i, idx in enumerate(idxs):
-        true_testsort = y_eval[idx,...].squeeze()
-        plot_testsort = state_out[idx,...].squeeze()
-        er_testsort = torch.abs(plot_testsort - true_testsort).squeeze()
-        
         plt.close()
-        fig = plt.figure(figsize=(15, 15))
-        grid = ImageGrid(fig, 211,
-                         nrows_ncols=(1,2),
-                         axes_pad=1.5,
-                         share_all=True,
-                         aspect=True,
-                         label_mode="L",
-                         cbar_location="right",
-                         cbar_mode="single",
-                         cbar_size="7%",
-                         cbar_pad=0.2
-                         )
-        
-        grid[0].contourf(XX, YY, plot_testsort, vmin=0, vmax=15, cmap=mpl.cm.viridis)
-        grid[0].set_title('FNO Output')
-        grid[0].set_xlabel(r'$x_1$')
-        grid[0].set_ylabel(r'$x_2$')
-        
-        ax01 = grid[1].contourf(XX, YY, true_testsort, vmin=0, vmax=15, cmap=mpl.cm.viridis)
-        grid[1].set_title('True Final State')
-        grid[1].set_xlabel(r'$x_1$')
-        grid[1].axes.yaxis.set_visible(False)
-        cb01 = grid[1].cax.colorbar(ax01)
-        grid[1].cax.toggle_label(True)
-        cb01.set_label(r'State')
-        
-        grid = ImageGrid(fig, 223,
-                         nrows_ncols=(1,1),
-                         aspect=False,
-                         label_mode="all"
-                         )
-        
-        grid[0].plot(torch.linspace(0, 1, s_eval), x_eval[idx, 0, :, 0].squeeze())
-        grid[0].grid(visible=True)
-        grid[0].set_xlim(0,1)
-        grid[0].set_ylim(1,5)
-        grid[0].set_title('Velocity Input Profile ' + '(' + names[i] + ')')
-        grid[0].set_xlabel(r'$x_1$')
-        grid[0].set_ylabel(r'$v_1(x_1, 0; \xi)$')
-        
-        grid = ImageGrid(fig, 224,
-                         nrows_ncols=(1,1),
-                         aspect=True,
-                         label_mode="all",
-                         cbar_location="right",
-                         cbar_mode="single",
-                         cbar_size="7%",
-                         cbar_pad=0.2
-                         )
-  
-        ax11 = grid[0].imshow(er_testsort, origin="lower", interpolation="spline16",\
-                               extent=(0,1,0,1), cmap=mpl.cm.viridis)
-        grid[0].set_title('Pointwise Absolute Error')
-        grid[0].set_xlabel(r'$x_1$')
-        grid[0].set_ylabel(r'$x_2$')
-        cb11 = grid[0].cax.colorbar(ax11)
-        grid[0].cax.toggle_label(True)
-        cb11.set_label(r'Error')
-        
-        # Save min, median, max error plots (contour)
+        plt.plot(torch.linspace(0, 1, s_eval), x_eval[idx, 0, :, 0].squeeze())
+        plt.grid()
+        plt.xlim([0,1])
+        plt.ylim([1,5])
+        plt.title('Velocity Input Profile ' + '(' + names[i] + ')')
+        plt.xlabel(r'$x_1$')
+        plt.ylabel(r'$v_1(x_1, 0; \xi)$')
+
         plt.savefig(plot_folder + "eval_" + names[i] + obj_suffix_eval[:-3] + "pdf", format='pdf')
     
     # Close open figure if running interactively
