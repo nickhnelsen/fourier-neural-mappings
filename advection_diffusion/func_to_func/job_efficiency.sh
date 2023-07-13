5c38522e4e27910d59a5e9d0daaa74366a38325c
nnelsen <nnelsen@caltech.edu>
2023-07-13 10:30:28 -0700

    ready for hpc loop

--- advection_diffusion/func_to_func/job_efficiency.sh
+++ advection_diffusion/func_to_func/job_efficiency.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-svpref=efficiency_LOOP_TEST_PATHS/
-datsuf=nu_1p5_ell_p25_torch/
-N=1000
+svpref=efficiency/
+declare -a dsl=("nu_1p5_ell_p25_torch/" "nu_inf_ell_p25_torch/" "nu_inf_ell_p05_torch/")
+declare -a Ns=("10" "32" "100" "316" "1000" "3162" "10000")
 d=1000
-model=FNO2d
+declare -a models=("FNO2d" "FNO1d2")
 declare -a Ls=("2" "4")
 m=12
 w=32
 m1d=24
 w1d=128
 
-dir_name="./results/${svpref}${datsuf}"
-mkdir -p ${dir_name}
-for L in "${Ls[@]}"; do
-    job_name="${svpref:0:1}_n${N}_d${d}_${model}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
-    std="${dir_name}R-%x.%j"
-    scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fno.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d}"
-    
-    echo "submit command: $scommand"
-    
-    $scommand
+for datsuf in "${dsl[@]}"; do
+    dir_name="./results/${svpref}${datsuf}"
+    mkdir -p ${dir_name}
+    for model in "${models[@]}"; do
+        for L in "${Ls[@]}"; do
+            for N in "${Ns[@]}"; do
+                job_name="${svpref:0:1}_n${N}_d${d}_${model}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
+                std="${dir_name}R-%x.%j"
+                scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fno.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d}"
+                
+                echo "submit command: $scommand"
+                
+                $scommand
+            done
+        done
+    done
 done
