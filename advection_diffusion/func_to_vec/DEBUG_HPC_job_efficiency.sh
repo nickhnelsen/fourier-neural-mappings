345dd98ca77ff2611a1db92e0223fec120eaeee1
nnelsen <nnelsen@caltech.edu>
2023-07-13 14:28:29 -0700

    add fnf scripts

---
+++ advection_diffusion/func_to_vec/DEBUG_HPC_job_efficiency.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+svpref=efficiency_DEBUG/
+declare -a dsl=("nu_1p5_ell_p25_torch/")
+declare -a Ns=("10")
+d=1000
+declare -a models=("FNF2d" "FNF1d")
+declare -a Ls=("2")
+m=12
+w=32
+m1d=24
+w1d=128
+qoi=012345
+
+COUNT=0
+for datsuf in "${dsl[@]}"; do
+    dir_name="./results/${svpref}${datsuf}"
+    mkdir -p ${dir_name}
+    for model in "${models[@]}"; do
+        for L in "${Ls[@]}"; do
+            for N in "${Ns[@]}"; do
+                job_name="${svpref:0:1}_n${N}_d${d}_${model}_qoi${qoi}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
+                std="${dir_name}R-%x.%j"
+                scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fnf.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d} ${qoi}"
+                
+                echo "submit command: $scommand"
+                
+                #$scommand
+                
+                (( COUNT++ ))
+            done
+        done
+    done
+done
+
+echo ${COUNT} jobs
