5db6a497722d430b0d421959ce622e2c8ecd60a3
nnelsen <nnelsen@caltech.edu>
2023-07-12 21:27:04 -0700

    bug

--- advection_diffusion/func_to_func/job_efficiency.sh
+++ advection_diffusion/func_to_func/job_efficiency.sh
@@ -16,7 +16,7 @@ mkdir -p ${dir_name}
 for L in "${Ls[@]}"; do
     job_name="${svpref:0:1}_n${N}_d${d}_${model}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
     std="${dir_name}R-%x.%j"
-    scommand="sbatch --job_name=${job_name} --output=${std}.out --error=${std}.err train_fno.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d}"
+    scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fno.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d}"
     
     echo "submit command: $scommand"
     
