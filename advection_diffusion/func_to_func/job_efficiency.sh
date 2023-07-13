b26f3fd809c57cbcef77a5a3ecc8bd9b167ef78e
nnelsen <nnelsen@caltech.edu>
2023-07-12 21:23:41 -0700

    paths

--- advection_diffusion/func_to_func/job_efficiency.sh
+++ advection_diffusion/func_to_func/job_efficiency.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-svpref=efficiency_LOOP_TEST/
+svpref=efficiency_LOOP_TEST_PATHS/
 datsuf=nu_1p5_ell_p25_torch/
 N=1000
 d=1000
@@ -11,8 +11,12 @@ w=32
 m1d=24
 w1d=128
 
+dir_name="./results/${svpref}${datsuf}"
+mkdir -p ${dir_name}
 for L in "${Ls[@]}"; do
-    scommand="sbatch train_fno.sbatch $svpref $datsuf $N $d $model $L $m $w $m1d $w1d"
+    job_name="${svpref:0:1}_n${N}_d${d}_${model}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
+    std="${dir_name}R-%x.%j"
+    scommand="sbatch --job_name=${job_name} --output=${std}.out --error=${std}.err train_fno.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d}"
     
     echo "submit command: $scommand"
     
