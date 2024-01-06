4793bb5fc3d59401f8c070b458b9642a2d633657
nnelsen <nnelsen@caltech.edu>
2024-01-06 15:51:15 -0800

    f2f paper job

---
+++ advection_diffusion/func_to_func/job_paper.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+svpref=paper/
+declare -a dsl=("nu_1p5_ell_p25_torch/")
+declare -a Ns=("10" "32" "100" "316" "1000" "3162" "10000")
+declare -a ds=("2" "20" "1000")
+declare -a models=("FNO2d")
+declare -a Ls=("4")
+m=12
+w=32
+m1d=12
+w1d=32
+
+COUNT=0
+for datsuf in "${dsl[@]}"; do
+    dir_name="./results/${svpref}${datsuf}"
+    mkdir -p ${dir_name}
+    for model in "${models[@]}"; do
+        for L in "${Ls[@]}"; do
+            for d in "${ds[@]}"; do
+                for N in "${Ns[@]}"; do
+                    job_name="${svpref:0:1}_n${N}_d${d}_${model}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
+                    std="${dir_name}R-%x.%j"
+                    scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fno.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d}"
+
+                    echo "submit command: $scommand"
+
+                    $scommand
+
+                    (( COUNT++ ))
+                done
+            done
+        done
+    done
+done
+
+echo ${COUNT} jobs
