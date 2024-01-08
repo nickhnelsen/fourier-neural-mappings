cbb45ebb58b685f28164ffe7c846dd967d204373
nnelsen <nnelsen@caltech.edu>
2024-01-07 18:21:24 -0800

    dnn hpc advectdif

--- advection_diffusion/dnn/job_hyper.sh
+++ advection_diffusion/dnn/job_hyper.sh
@@ -4,11 +4,12 @@ svpref=hyper/
 declare -a dsl=("nu_1p5_ell_p25_torch/")
 declare -a Ns=("3162")
 declare -a models=("DNN")
-declare -a Ls=("3" "4")
-declare -a modes=("3" "6" "12" "18" "24" "36")
-declare -a constants=("144" "288" "576" "1152")
+declare -a Ls=("3" "4" "5")
+declare -a widths=("16" "32" "64" "128" "256" "512" "1024" "2048")
 declare -a qois=("1234")
 d=1000
+m=0
+m1d=0
 
 COUNT=0
 for datsuf in "${dsl[@]}"; do
@@ -18,21 +19,17 @@ for datsuf in "${dsl[@]}"; do
         for L in "${Ls[@]}"; do
             for qoi in "${qois[@]}"; do
                 for N in "${Ns[@]}"; do
-                    for const in "${constants[@]}"; do
-                        for m in "${modes[@]}"; do
-                            w=$((const / m))
-                            w1d=$w
-                            m1d=$m
-                            job_name="${svpref:0:1}_n${N}_d${d}_${model}_qoi${qoi}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
-                            std="${dir_name}R-%x.%j"
-                            scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fnn.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d} ${qoi}"
+                    for w in "${widths[@]}"; do
+                        w1d=$w
+                        job_name="${svpref:0:1}_n${N}_d${d}_${model}_qoi${qoi}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
+                        std="${dir_name}R-%x.%j"
+                        scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fnn.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d} ${qoi}"
 
-                            echo "submit command: $scommand"
+                        echo "submit command: $scommand"
 
-                            $scommand
+                        $scommand
 
-                            (( COUNT++ ))
-                        done
+                        (( COUNT++ ))
                     done
                 done
             done
