19b0ffafa8209e1a96ba444652b3b7c30f26442b
nnelsen <nnelsen@caltech.edu>
2024-01-07 18:25:47 -0800

    fix typo

--- advection_diffusion/dnn/job_hyper.sh
+++ advection_diffusion/dnn/job_hyper.sh
@@ -23,7 +23,7 @@ for datsuf in "${dsl[@]}"; do
                         w1d=$w
                         job_name="${svpref:0:1}_n${N}_d${d}_${model}_qoi${qoi}_L${L}_m${m}_w${w}_md${m1d}_wd${w1d}_${datsuf::-7}"
                         std="${dir_name}R-%x.%j"
-                        scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_fnn.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d} ${qoi}"
+                        scommand="sbatch --job-name=${job_name} --output=${std}.out --error=${std}.err train_dnn.sbatch ${svpref} ${datsuf} ${N} ${d} ${model} ${L} ${m} ${w} ${m1d} ${w1d} ${qoi}"
 
                         echo "submit command: $scommand"
 
